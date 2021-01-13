2f4a4dc7cdca9ab155576c784de3d103ae737169
jnorbi99 <jnorbert1999@gmail.com>
2021-01-13 12:49:54 +0100

    Change state in MenuComponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,15 +8,9 @@ class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedDish: null
-        }
-        console.log('Menu Component construktor is invoked');
     }
 
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish});
-    }
+    
 
     
 
@@ -28,7 +22,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>                            
                         <CardImgOverlay>
                             <CardTitle heading>{dish.name}</CardTitle>
