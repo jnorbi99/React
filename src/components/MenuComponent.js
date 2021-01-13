d9379dc90c9ded4aca2985d6d6d4446812c1a123
jnorbi99 <jnorbert1999@gmail.com>
2021-01-13 13:49:58 +0100

    Add onClick method body

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-import DishDetail from './DishdetailComponent';
 
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
-
     }
-
-    
-
-    
 
     componentDidMount() {
         console.log('Menu Component componentDidMount is invoked');
@@ -22,7 +16,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>                            
                         <CardImgOverlay>
                             <CardTitle heading>{dish.name}</CardTitle>
@@ -39,7 +33,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail selectedDish = {this.state.selectedDish}/>
             </div>
         );
     }
