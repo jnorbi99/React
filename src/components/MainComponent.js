eedac976396894d9682b87e2b1db847002a0f3ea
jnorbi99 <jnorbert1999@gmail.com>
2021-01-15 13:23:48 +0100

    2. Assignment Task 1.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { Switch, Route, Redirect} from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
@@ -53,6 +54,7 @@ class Main extends Component{
                 <Header />
                     <Switch>
                         <Route path="/home" component={HomePage} />
+                        <Route path="/aboutus" component={() => <About leaders={this.state.leaders} /> } />
                         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                         <Route path="/menu/:dishId" component={DishWithId} />
                         <Route exact path="/contactus" component={Contact} />
