cddc0603f753d25b9d3f196df67d6a681aba1721
jnorbi99 <jnorbert1999@gmail.com>
2021-01-25 14:17:58 +0100

    addComment -> pstComment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,7 +30,7 @@ import { baseUrl } from '../shared/baseUrl';
 
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render() {
@@ -135,7 +135,7 @@ import { baseUrl } from '../shared/baseUrl';
         }
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if(comments != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -150,7 +150,7 @@ import { baseUrl } from '../shared/baseUrl';
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
         } else {
@@ -194,7 +194,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <div className="row">
                             <RenderDish dish={props.selectedDish}/>
                             <RenderComments comments={props.comments}
-                                addComment={props.addComment}
+                                postComment={props.postComment}
                                 dishId={props.selectedDish.id} />
                     </div>
                 </div>
